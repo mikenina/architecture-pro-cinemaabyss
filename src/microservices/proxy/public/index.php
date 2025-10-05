eab758cc026b275156f9c2493da2a5123aac2b94
Olga Mikenina <omikenina@calltouch.net>
2025-10-05 05:05:17 +0300

    fix config

--- src/microservices/proxy/public/index.php
+++ src/microservices/proxy/public/index.php
@@ -167,10 +167,10 @@ class Curler
             [
                 CURLOPT_URL => $path,
                 CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_VERBOSE => true,
                 CURLOPT_TIMEOUT => 30,
             ]
         );
-
         $responseBody = (string) curl_exec($ch);
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         $curlErrorMessage = curl_error($ch);
